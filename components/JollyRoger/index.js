668b88d77a10f4b99c4017f26b241d93c3ea3fb9
devkun69 <devkun69@gmail.com>
2024-06-10 23:15:21 +0800

    refactor: optimize codes

--- components/JollyRoger/index.js
+++ components/JollyRoger/index.js
@@ -7,18 +7,22 @@ const JollyRoger = () => {
     return (
         <div className={styles.onePiece}>
             <div className={styles.bones}>
-                <div className={styles.bone}/>
-                <div className={styles.bone}/>
-                <div className={styles.bone}/>
-                <div className={styles.bone}/>
+                {
+                    Array.from({length: 4}).map((_, index) => {
+                        return (<div key={index} className={styles.bone}/>)
+                    })
+                }
             </div>
             <div className={styles.head}>
                 <div className={styles.hat}>
                     <div className={styles.lineLeft}/>
                     <div className={styles.lineRight}/>
                 </div>
-                <div className={styles.eye}/>
-                <div className={styles.eye}/>
+                {
+                    Array.from({length: 2}).map((_, index) => {
+                        return (<div key={index} className={styles.eye}/>);
+                    })
+                }
             </div>
             <div className={styles.mouth}>
                 <div className={styles.teeth}/>
